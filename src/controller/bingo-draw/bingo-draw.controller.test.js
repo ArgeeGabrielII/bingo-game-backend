193fdd6c7f650a5ee2968eece3830d1f74b1fe1f
Rosalito gabriel <rosalitogabriel@solaireresort.com>
2023-07-26 15:50:57 +0800

    Updated Test Cases

--- src/controller/bingo-draw/bingo-draw.controller.test.js
+++ src/controller/bingo-draw/bingo-draw.controller.test.js
@@ -1,11 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const conDraw = require('./bingo-draw.controller');
+// const conDraw = require('./bingo-draw.controller');
 
 describe('bingo-draw.controller.js', () => {
     context('Check Number Draw', async () => {
-        const resDraw = await conDraw.drawBingo();
+        // const resDraw = await conDraw.drawBingo();
 
         before(() => {
             console.log(`    [CROSS ENV] NODE_ENV: ${process.env.NODE_ENV}`);
